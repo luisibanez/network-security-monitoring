7d2e53b00cae31cdbe9da07c4c50bc8b5d2a4683
Luis Ibanez <luisi@google.com>
2021-12-06 17:21:57 -0600

    Remove 8.8.4.4 from DNS summary doc since it is a constant.

--- dns_queries_summary.sh
+++ dns_queries_summary.sh
@@ -10,5 +10,6 @@ ZEEK_CUT=/usr/local/zeek/bin/zeek-cut
 cat $1 \
   | ${ZEEK_CUT} -d id.resp_h query \
   | sort | uniq -c | sort -g -r \
-  | grep "8.8.4.4" 
+  | grep "8.8.4.4" \
+  | sed 's/8.8.4.4\t//'
 
