75d2b768e269483b574b13249643b95412d911e9
Luis Ibanez <luisi@google.com>
2022-08-01 17:49:36 -0500

    Add script to compute entropy of extracted files.
    For each one of the files in a given directory, invoke the ent tool to compute entropy.

---
+++ entropy_extracted_files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+#  Compute entropy on  all extracted files
+#
+#  Usage: entropy_extracted_fies.sh   EXTRACTED_FILES_DIRECTORY
+#
+
+echo "Computing Entropy of extracted files ..." 
+
+#
+#  For each one of the files in the directory $1
+#       Print out the filename first, without a newline (-n flag)
+#       Print out the output of the ent (entropy) command in tabular form (-t flag).
+#  Remove the headers that ent introduces when using the -t flag 
+#  Sort by value of entropy (-k 4 field), from comma separated lines (-t',')
+#  Write out the output in 'entropy.csv' file
+#
+find $1 -name 'extract*'      \
+   -exec echo -n {} \;        \
+   -exec ent -t  {} \;        \
+   |  awk '/Correlation$/ { gsub(/0\,File\-bytes.*Serial\-Correlation/,","); printf("%s", $0); next } 1'   \
+   |  sort -t',' -k 4    \
+   |  tee  entropy.csv
+
